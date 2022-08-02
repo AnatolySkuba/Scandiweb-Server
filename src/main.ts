c50011101285ddbb4b9a14d5337bb1b279807e4c
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-08-02 12:14:36 +0300

    make it better

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from "apollo-server";
 
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
 
 const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
+	typeDefs: typeDefs,
+	resolvers: resolvers,
 });
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+	console.log(`
+    🚀  Server is ready at ${url}
+    📭  Query at https://studio.apollographql.com/dev
+  `);
 });
+
+// server.listen().then(({ url }) => {
+//     console.log(`🚀  Server ready at ${url}`);
+// });
