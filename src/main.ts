aea3f6274a403d2b57b1e06f971aef15ff47f96a
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-12-10 20:45:04 +0200

    s

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { ApolloServer } from "apollo-server";
-
+// const { ApolloServer } = require("apollo-server");
+// const typeDefs = require('./schema');
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
 
 const server = new ApolloServer({
-	typeDefs: typeDefs,
-	resolvers: resolvers,
+    typeDefs: typeDefs,
+    resolvers: resolvers,
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-	console.log(`
+    console.log(`
     🚀  Server is ready at ${url}
     📭  Query at https://studio.apollographql.com/dev
   `);
